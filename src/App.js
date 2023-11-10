723349d858d933b029f4f80df0302229085beecb
Benja203 <benjamin.alvarado.1510@gmail>
2023-11-10 17:57:12 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Instituto de Educación Pública - Honorio Delgado Espinoza
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Claudia Amparo Puma Ticona - Turno: Mañana
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
